271f3bc7d954dd19ca3edecb2c0ae813b35eb1c4
victors3136 <115093754+victors3136@users.noreply.github.com>
2024-05-14 18:54:13 +0300

    update to table structure -- making names manageable

--- creating_tables.sql
+++ creating_tables.sql
@@ -1,42 +1,41 @@
-create table "user"(
-	"id" uuid,
-	"username" varchar(32),
-	"password" varchar(64),
-	"email" varchar(255),
-	constraint pk_user primary key ("id"),
-	constraint username_not_null check("username" is not null),
-	constraint password_not_null check("password" is not null),
-	constraint unique_username unique("username")
+create table sdi_user(
+	u_id uuid,
+	u_username varchar(32),
+	u_password varchar(64),
+	u_email varchar(255),
+	constraint pk_user primary key (u_id),
+	constraint username_not_null check(u_username is not null),
+	constraint password_not_null check(u_password is not null),
+	constraint unique_username unique(u_username)
 );
-create table "task"(
-	"id" uuid, 
-	"name" varchar(255),
-	"description" text,
-	"priority" smallint,
-	"due_date" timestamp,
-	"user" uuid,
-	constraint pk_tasks primary key("id"),
-	constraint task_name_not_null check("name" is not null),
-	constraint task_priority_between_1_and_10 check(1<= "priority" and "priority" <= 10),
-	constraint task_user_not_null check("user" is not null),
-	constraint fk_users_tasks foreign key ("user") references "user"("id") on delete cascade
+create table sdi_task(
+	t_id uuid, 
+	t_name varchar(255),
+	t_description text,
+	t_priority smallint,
+	t_due_date timestamp,
+	t_user uuid,
+	constraint pk_tasks primary key(t_id),
+	constraint task_name_not_null check(t_name is not null),
+	constraint task_priority_between_1_and_10 check(1<= t_priority and t_priority <= 10),
+	constraint task_user_not_null check(t_user is not null),
+	constraint fk_users_tasks foreign key(t_user) references sdi_user(u_id) on delete cascade
 );
-create index index_on_user_id_for_task on task("user");
-create table subtask(
-	"id" UUID, 
-	"subject" varchar(255),
-	"task" UUID,
-	constraint pk_subtasks primary key("id"),
-	constraint fk_tasks_subtasks foreign key("task") references "task"("id") on delete cascade,
-	constraint subtask_subject_not_null check("subject" is not null)
+create index index_on_user_id_for_task on sdi_task(t_user);
+create table sdi_subtask(
+	s_id uuid, 
+	s_subject varchar(255),
+	s_task uuid,
+	constraint pk_subtasks primary key(s_id),
+	constraint fk_tasks_subtasks foreign key(s_task) references sdi_task(t_id) on delete cascade,
+	constraint subtask_subject_not_null check(s_subject is not null)
 );
-create index index_on_task_id_for_subtask on "subtask"("task");
-
+create index index_on_task_id_for_subtask on sdi_subtask(s_task);
 select * 
-	from "user" "u" 
-		inner join "task" "t" on "u"."id" = "t"."user" 
-		inner join "subtask" "s" on "t"."id" = "s"."id";
+	from sdi_user 
+		inner join sdi_task on u_id = t_user 
+		inner join sdi_subtask on t_id = s_task;
 	
-drop table "subtask";
-drop table "task";
-drop table "user";
+drop table sdi_subtask cascade;
+drop table sdi_task cascade;
+drop table sdi_user cascade;
