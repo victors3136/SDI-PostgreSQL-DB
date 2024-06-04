42e8402d04123f8e12ee949d99e10e2710448dbb
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-06-04 19:23:02 +0300

    aaaaaaaaaaaaaaaaaaaa

--- update_user_to_account_for_role.sql
+++ update_user_to_account_for_role.sql
@@ -10,3 +10,11 @@ from sdi_user
 	inner join sdi_role_permission on r_id = rp_role
 	inner join sdi_permission on rp_permission = p_id
 group by "id", "username", "role";
+
+
+select p_name
+from sdi_permission
+         inner join sdi_role_permission on p_id = rp_permission
+         inner join sdi_role on rp_role = r_id
+         inner join sdi_user on u_role = r_id
+where u_id = 'd1daab41-2584-4412-8d80-382ca49e0346'
